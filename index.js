8043e512f38975d9c026e442ec5b7a3821d9ab9e
a1ext0 <a1sto@ya.ru>
2020-04-21 23:02:15 +0300

    Исправлены ошибки

--- index.js
+++ index.js
@@ -24,25 +24,68 @@ io.on('connection', socket => {
   });
 
   socket.on('saveData', data => {
-    connection.query(`INSERT INTO Canvas (DifferenceFromCompetitorsWindow,TheSolutionWeOffer,KeyResources,keyMetrics,ExpenditureStructure,ForNotes,TaskForce,ClientProblem,ClientTasks,HowToDecideNow,IncomeStream, name) values('${data.DifferenceFromCompetitorsWindow}', '${data.TheSolutionWeOffer}', '${data.KeyResources}', '${data.keyMetrics}', '${data.ExpenditureStructure}', '${data.ForNotes}', '${data.TaskForce}', '${data.ClientProblem}', '${data.ClientTasks}', '${data.HowToDecideNow}', '${data.IncomeStream}','${data.name}')`, (err, data, fields)=> {
-      if (err) {
-        console.error(err);
-        io.sockets.emit('saveDataStatus', {status: 0});
+    let allow = true;
+    for (let string in data) {
+      if (typeof data[string] == 'string') {
+        data[string] = sqlCheck.quotes(data[string]);
       } else {
-        io.sockets.emit('saveDataStatus', {status: 1});
+        allow = false;
       }
-    });
+    }
+    if (allow) {
+      connection.query(`INSERT INTO Canvas (DifferenceFromCompetitorsWindow,TheSolutionWeOffer,KeyResources,keyMetrics,ExpenditureStructure,ForNotes,TaskForce,ClientProblem,ClientTasks,HowToDecideNow,IncomeStream, name, status) values('${data.DifferenceFromCompetitorsWindow}', '${data.TheSolutionWeOffer}', '${data.KeyResources}', '${data.keyMetrics}', '${data.ExpenditureStructure}', '${data.ForNotes}', '${data.TaskForce}', '${data.ClientProblem}', '${data.ClientTasks}', '${data.HowToDecideNow}', '${data.IncomeStream}','${data.name}', '${data.status}')`, (err, data, fields)=> {
+        if (err) {
+          console.error(err);
+          io.sockets.emit('saveDataStatus', {status: 0});
+        } else {
+          io.sockets.emit('saveDataStatus', {status: 1});
+        }
+      });
+    } else {
+      io.sockets.emit('saveDataStatus', {status: 0, data: 'badRequest'});
+    }
   });
 
   socket.on('updateData', data => {
-    connection.query(`UPDATE Canvas SET ${data.key} = '${data.value}' WHERE id = ${data.id}`, (err, data, fields)=> {
-      if (err) {
-        console.error(err);
-        io.sockets.emit('updateDataStatus', {status: 0});
+    let allow = true;
+    for (let string in data) {
+      if (typeof data[string] == 'string') {
+        data[string] = sqlCheck.quotes(data[string]);
       } else {
-        io.sockets.emit('updateDataStatus', {status: 1});
+        allow = false;
       }
-    });
+    }
+    if (allow) {
+      if (!data.id) {
+        console.log('new Canvas');
+        connection.query(`INSERT INTO Canvas (${data.key}, status) values('${data.value}', 0)`, (err, data, fields)=> {
+          if (err) {
+            console.error(err);
+            io.sockets.emit('updateDataStatus', {status: 0});
+          } else {
+            io.sockets.emit('updateDataStatus', {status: 1, id: data.insertId});
+          }
+        });
+      } else {
+        connection.query(`UPDATE Canvas SET ${data.key} = '${data.value}', status = 0 WHERE id = ${data.id}`, (err, data, fields)=> {
+          if (err) {
+            console.error(err);
+            io.sockets.emit('updateDataStatus', {status: 0});
+          } else {
+            io.sockets.emit('updateDataStatus', {status: 1});
+          }
+        });
+      }
+    } else {
+      io.sockets.emit('updateDataStatus', {status: 0, data: 'badRequest'});
+    }
   });
 });
 io.listen(45705);
+
+const sqlCheck = {
+  quotes: function functionName(string) {
+    let s = string.replace(/'/g, '');
+    return s;
+  }
+};
